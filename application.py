e67b8a25c75ee6a3c5f7ac45f344e72d29ecfca4
ouersl <2569792062@qq.com>
2020-04-20 17:27:37 +0800

    '自定义服务器'

--- application.py
+++ application.py
@@ -1,5 +1,13 @@
 # 封装了Flask的全局变量，app，sql等配置
+import os
 
 from flask import Flask
+from flask_script import Manager
 
-app = Flask(__name__)
+class Applciation(Flask):
+    def __init__(self,import_name,template_folder=None,root_path=None):
+        super(Applciation, self).__init__(import_name,template_folder=template_folder,root_path=root_path)
+
+
+app = Applciation(__name__,template_folder=os.getcwd()+'/web/templates/',root_path=os.getcwd())
+manager = Manager(app)
