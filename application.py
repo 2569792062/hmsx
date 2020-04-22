ea5ea0128eae7b004970192c698a9b7c60aa9518
ouersl <2569792062@qq.com>
2020-04-22 15:56:42 +0800

    基础架构

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
-# 封装了Flask的全局变量，app，sql等配置
-import os
-
 from flask import Flask
 from flask_script import Manager
+import os
 
 class Applciation(Flask):
     def __init__(self,import_name,template_folder=None,root_path=None):
-        super(Applciation, self).__init__(import_name,template_folder=template_folder,root_path=root_path)
+        super(Applciation, self).__init__(import_name,template_folder=template_folder,root_path=root_path,static_folder=None)
 
 
 app = Applciation(__name__,template_folder=os.getcwd()+'/web/templates/',root_path=os.getcwd())
 manager = Manager(app)
+
+
+from common.libs.UrlManager import UrlManager
+app.add_template_global(UrlManager.buildStaticUrl,'buildStaticUrl')
+app.add_template_global(UrlManager.buildUrl,'buildUrl')
